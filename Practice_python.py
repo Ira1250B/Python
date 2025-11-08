6e97a892da6bf2c3f337def3d914b0d37e4e8cd7
Ira1250B <ira.bhogayata@gmail.com>
2025-11-08 22:02:32 +0530

    Clean up Practice_python.py and add array generation
    Removed commented-out code and added numpy array generation.

--- Practice_python.py
+++ Practice_python.py
@@ -1,18 +1,1 @@
-# b=4
-# print("a a^2 a^3 \n")
-# for i in range(1,b):
-#     print(i," ",i**2," ",i**3," ","\n")
-# s1=input("string\n")
-# s=s1.split()
-# print("length of longest word is ",len(s[len(s)-1]))
-# l1=[ [1,2],
-#      [3,4],
-#      [5,6],
-#      [7,8],
-#      [9,10]]
-# print([item  for List in l1  for item in List])
-import numpy as np
-# t=(2,45,67,1,8,5.6,78,90,34)
-# print(tuple(sorted(t)))
-arr=np.linspace(0,3,5)
-print(arr)
+
